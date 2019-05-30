90e0af9d8cc7e1d256acd3227934bc51c98cdbcd
Sean Murphy <filmmakersean@fastmail.net>
2019-05-30 23:56:57 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -72,7 +72,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday|
-    puts "#{season.to_s.capitalize} :"
+    puts "#{season.to_s.capitalize}:"
     holiday.each do |holiday, supplies|
       holiday_array = holiday.to_s.split('_')
       holiday_array_fixed = []
