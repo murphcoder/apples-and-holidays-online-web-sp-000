5dd8ab34c4e7db61b8f3bcbb62fb6e6ffa739bb6
Sean Murphy <filmmakersean@fastmail.net>
2019-05-30 23:45:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,6 +74,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday|
     puts "#{season.to_s.capitalize} :"
     holiday.each do |holiday, supplies|
+      holiday_array = holiday.split
       
     end
   end
