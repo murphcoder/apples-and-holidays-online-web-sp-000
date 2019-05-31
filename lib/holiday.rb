d9a4018c76af45156c07e2e7233e43bd77a906f9
Sean Murphy <filmmakersean@fastmail.net>
2019-05-31 00:01:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -87,7 +87,17 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_with_bbq = []
+  holiday_hash.each do |season, holiday|
+    holiday.each do |holiday, supplies|
+      supplies.each do |supply|
+        if supply == "BBQ"
+          holiday_with_bbq << holiday
+        end
+      end
+    end
+  end
+  holiday_with_bbq
 end
 
 
